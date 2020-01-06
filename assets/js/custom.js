4903e7836225aa2cd892b7332da74b1fe5f7de02
Arnaud TANGUY <arnaud.tanguy@lirmm.fr>
2020-01-06 17:57:31 +0900

    Initial config, image carousel, first article

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,11 @@
 // Scroll Top
-$('.top').click(function() {
-  $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
-});
 $(window).scroll(function() {
   if ($(this).scrollTop() > $(window).height()) {
     $('.top').addClass("up");
+    $('.top').click(function() {
+      $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+      console.log("TOP");
+    });
   } else {
     $('.top').removeClass("up");
   }
